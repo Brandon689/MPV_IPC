295352765c1113392ce22ce0137105778a3a2cc2
Brandon <brandon139@protonmail.com>
2023-11-07 22:27:21 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
+using System.Threading.Tasks;
+
+class MpvIpcExample
+{
+    private static async Task SendCommandAsync(NamedPipeClientStream pipeStream, string command)
+    {
+        // Ensure the command is in JSON format and terminated with a newline
+        byte[] buffer = Encoding.UTF8.GetBytes(command + "\n");
+        await pipeStream.WriteAsync(buffer, 0, buffer.Length);
+        await pipeStream.FlushAsync();
+
+        Console.WriteLine("Command sent: " + command);
+    }
+
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "mpv-pipe", PipeDirection.Out))
+            {
+                Console.WriteLine("Connecting to MPV IPC...");
+                pipeClient.Connect(); // Connect to the pipe
+                Console.WriteLine("Connected to MPV IPC.");
+
+                // Example command to set the volume
+                string setVolumeCommand = "{\"command\": [\"set_property\", \"volume\", 75]}";
+                await SendCommandAsync(pipeClient, setVolumeCommand);
+
+                Console.WriteLine("Commands executed. Exiting...");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+        }
+    }
+}
